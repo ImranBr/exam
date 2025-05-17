eb4bde13bfb68ce03612d3847488bd267f129ba2
Imran Barbouch <imranbarbouch@hotmail.fr>
2025-05-17 16:07:06 +0200

    main gnl

--- gnl/test/get_next_line.c
+++ gnl/test/get_next_line.c
@@ -1,14 +1,12 @@
 #include "get_next_line.h"
 
-char    *ft_strdup(char *src)
+char *ft_strdup(char *src)
 {
     int i = 0;
     char *dest;
     while (src[i])
         i++;
     dest = malloc(sizeof(char) * (i + 1));
-    if (!dest)
-        return (NULL);
     i = 0;
     while (src[i])
     {
@@ -22,10 +20,10 @@ char    *ft_strdup(char *src)
 char    *get_next_line(int fd)
 {
     static char buffer[BUFFER_SIZE];
-    static int  buffer_pos = 0;
-    static int  pos = 0;
-    char        line[70000];
-    int         i = 0;
+    static int buffer_pos = 0;
+    static int pos = 0;
+    char line[70000];
+    int i = 0;
     if (fd < 0 || BUFFER_SIZE <= 0)
         return (NULL);
     while (1)
@@ -45,4 +43,20 @@ char    *get_next_line(int fd)
         return (NULL);
     line[i] = '\0';
     return (ft_strdup(line));
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	int fd = open("test.txt", O_RDONLY);
+	char *line;
+
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
 }
--- gnl/test/get_next_line.h
+++ gnl/test/get_next_line.h
@@ -7,5 +7,6 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 
 #endif
